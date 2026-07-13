cf46d857d6f9de60824c0090ee7e84e1f4fba987
idebenito5-droid <idebenito5@gmail.com>
2026-07-13 23:06:49 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE = 'talmut-v1';
+var ASSETS = [
+  '/Talmut/',
+  '/Talmut/index.html',
+  '/Talmut/manifest.json',
+  '/Talmut/Logo_Talmut.png',
+  '/Talmut/Talmut.png',
+  '/Talmut/Blue_Lagoon.png',
+  '/Talmut/Histórico.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
